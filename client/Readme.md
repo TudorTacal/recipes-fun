4ae6cff9fa1356128381f16a23c7727ad3d9762d
TudorTacal <tudortacal@gmail.com>
2020-02-24 12:29:19 +0000

    Setup Apollo Client

---
+++ client/Readme.md
@@ -0,0 +1,26 @@
+Now URL https://recipes-organizer.now.sh/
+
+GraphQL integration
+https://blog.apollographql.com/whats-next-js-for-apollo-e4dfe835d070
+https://github.com/zeit/next.js/tree/master/examples/with-apollo
+https://github.com/benawad/typescript-nextjs-graphql-series/blob/1_apollo_setup/lib/initApollo.ts
+
+Design system
+https://material-ui.com/components/grid/
+
+Next Mongo GrapqhQL
+https://www.mongodb.com/blog/post/introducing-graphql-support-in-mongodb-atlas-with-stitch
+https://www.compose.com/articles/using-graphql-with-mongodb/
+https://github.com/maxeuler/nextJS-graphQL-mongoDB-starter
+
+Tasks
+1. Hook-up MongoDB through GraphQL
+2. Create a form to submit a receipe info
+
+Use Apollo Client on top of MongoDB. No GraphQL server to start with.
+
+Features
+ - make a page where you can find the health benefits and properties of each ingredient
+
+
+
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,36 @@
+// https://recipes-organizer.now.sh/
 import { NextPage } from 'next';
-
+import { useState } from 'react';
 interface Props {
 
 }
 
-const RecipesPage: NextPage<Props> = () => <div>Recipes</div>;
-// deploy the app
-export default RecipesPage;
+const Home: NextPage<Props> = () => {
+  const [input, setInput] = useState({});
+
+  function handleChange(event) {
+    const { target } = event;
+    const { name, value } = target;
+    setInput({ ...input, [name]: value });
+  }
+
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <form>
+        <label>Name</label>
+        <input name="name" onChange={handleChange} />
+        <label>Ingredient</label>
+        <input name="ingredient" onChange={handleChange}/>
+        <label>Directions</label>
+        <input name="quantity" onChange={handleChange} />
+        <label>Price</label>
+        <input name="price" onChange={handleChange} />
+        <label>Quantity</label>
+        <input name="directions" onChange={handleChange} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+export default Home;
